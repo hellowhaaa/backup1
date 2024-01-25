6c207fad1973f24bf5391e1d71c20679749f458e
Lily Chou <lily28252005@gmail.com>
2024-01-25 21:01:50 +0800

    XHR 測試, 尚未解決Load Json File

---
+++ xhr測試/xhr.js
@@ -0,0 +1,7 @@
+// const getBn = document.getElementById('get-btn');
+// const postBtn = document.getElementById('post-btn');
+
+
+
+const sendData = () => { };
+// postBtn.addEventListener('click', sendData);

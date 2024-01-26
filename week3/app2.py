1dba771ea1819bd3fda3e8fd9586e4ed7f7f69cc
Lily Chou <lily28252005@gmail.com>
2024-01-26 16:18:49 +0800

    Initial step of myname.html & set cookie

--- week3/app2.py
+++ week3/app2.py
@@ -3,46 +3,48 @@ from flask import (Flask, redirect, url_for, session, render_template,
 
 import forms
 import models
-from forms import SumUpForm
+
 import json
 
 DEBUG = True
 LOCAL_HOST = ''
 
 app = Flask(__name__)
-app.secret_key = 'sdfjiohdgssdkghnlsdhildfs'
+app.secret_key = 'sdfjiohdgssdkghnlsdhidfs'
 
-tpe = {
-    "id": 0,
-    "city_name": "Taipei",
-    "country_name": "Taiwan",
-    "is_capital": True,
-    "location": {
-        "longitude": 121.569649,
-        "latitude": 25.036786
-    }
-}
-nyc = {
-    "id": 1,
-    "city_name": "New York",
-    "country_name": "United States",
-    "is_capital": False,
-    "location": {
-        "longitude": -74.004364,
-        "latitude": 40.710405
-    }
-}
-ldn = {
-    "id": 2,
-    "city_name": "London",
-    "country_name": "United Kingdom",
-    "is_capital": True,
-    "location": {
-        "longitude": -0.114089,
-        "latitude": 51.507497
-    }
-}
-cities = [tpe, nyc, ldn]
+
+
+# tpe = {
+#     "id": 0,
+#     "city_name": "Taipei",
+#     "country_name": "Taiwan",
+#     "is_capital": True,
+#     "location": {
+#         "longitude": 121.569649,
+#         "latitude": 25.036786
+#     }
+# }
+# nyc = {
+#     "id": 1,
+#     "city_name": "New York",
+#     "country_name": "United States",
+#     "is_capital": False,
+#     "location": {
+#         "longitude": -74.004364,
+#         "latitude": 40.710405
+#     }
+# }
+# ldn = {
+#     "id": 2,
+#     "city_name": "London",
+#     "country_name": "United Kingdom",
+#     "is_capital": True,
+#     "location": {
+#         "longitude": -0.114089,
+#         "latitude": 51.507497
+#     }
+# }
+# cities = [tpe, nyc, ldn]
 
 @app.route('/')
 def index():
@@ -80,18 +82,40 @@ def sum():
             # return render_template('sum.html',number = number)
     return render_template('sum.html')
 
-@app.route('/result',methods=['GET'])
-def result():
-    # number = request.args.get('number')  # 從HTML 抓 name= number 
+# @app.route('/result',methods=['GET'])
+# def result():
+#     # number = request.args.get('number')  # 從HTML 抓 name= number 
 
-    return jsonify(cities)
+#     return jsonify(cities)
 
-@app.route('/<myName>')
-def my_name(myName):
-    return render_template('myname.html', myName = myName)
+@app.route('/myname', methods=['GET','POST'])
+def myname():
+    data = get_saved_data()
+    return render_template('myname.html',saves=data)
+
+# @app.route('/getcookie')
+# def getcookie():
+#    name = request.cookies.get('userID')
+#    return '<h1>welcome ' + name + '</h1>'
+
+@app.route('/save', methods=['POST'])
+def save():
+    # import pdb; pdb.set_trace()
+    response = make_response(redirect(url_for('myname')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('Names', json.dumps(data))
+    # request.form -> immutable Multi Dict -> items() -> tuple key value pair
+    # 轉乘json string
+    return response
 
 
-
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('Names')) #-> 又變回python dict
+    except TypeError: #若不是cookie得到我們不能轉成jason的東西的話
+        data = {}
+    return data
 
 
 
